4d71ec1f64054c7883c6b6a60cab331c01581f65
hommat <mateuszziomek2000@gmail.com>
2020-09-13 22:22:35 +0200

    Add single shoes page

--- src/Controller/ShoesController.php
+++ src/Controller/ShoesController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Shoes;
 use App\Repository\ShoesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -20,17 +22,30 @@ class ShoesController extends AbstractController
     }
 
     /**
-     * @Route("/list", name="_list")
+     * @Route("/", name="_list")
      */
     public function list(Request $request)
     {
         $offset = max(0, $request->query->getInt('offset'));
         $paginator = $this->shoesRepository->getShoesPaginator($offset);
 
-        return $this->render('shoes/index.html.twig', [
+        return $this->render('shoes/list.html.twig', [
             'shoesList' => $paginator,
             'previous' => $offset - ShoesRepository::PAGINATOR_PER_PAGE,
             'next' => min(count($paginator), $offset + ShoesRepository::PAGINATOR_PER_PAGE)
         ]);
+    }
+
+    /**
+     * @Route("/{slug}", name="_single")
+     */
+    public function single(string $slug)
+    {
+        $shoes = $this->shoesRepository->findOneBy(['slug' => $slug]);
+        if (!$shoes) {
+            throw new NotFoundHttpException('Shoes do not exist');
+        }
+
+        return $this->render('shoes/single.html.twig', ['shoes' => $shoes]);
     }
 }
